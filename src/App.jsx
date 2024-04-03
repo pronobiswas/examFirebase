7e02c612fbf7bd125e393931ad6ad35b3400276b
PRONOB BISWAS <biswaspronob@outlook.com>
2024-04-04 04:22:36 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
 
 import { useState } from 'react';
 import './App.css'
-import { getDatabase, ref, set } from "firebase/database";
+import { getDatabase, ref, set ,push } from "firebase/database";
 
 function App() {
 
   let [person , setPerson] = useState({
     fullname :"",
     roll : "" , 
-    selectOption : ""
+    selectOption : "",
+    design : ""
 
   });
   let [personDetail, setPersonDetail] = useState([]);
+  let [checkBoxValues , setCheckBoxValues] = useState([]);;
   const db = getDatabase();
 
 
   let handleInput = (e)=>{
-    let {name , value} = e.target;
-    setPerson({...person , [name]  : value})
+    let {name , value ,checked} = e.target;
+    setPerson({...person , [name]  : value});
+    if(checked){
+     setCheckBoxValues(pre => [...pre ,  value])
+   }else{
+    return [...checkBoxValues.filter(skill => skill == value)]
+   }
+   console.log(checkBoxValues);
   };
 
   let handleSubmit = ()=>{
@@ -26,12 +34,13 @@ function App() {
     setPersonDetail(personDetailList)
     
 
-    set(ref(db, 'users'), {
-    nam : personDetail
+    set(push(ref(db, 'users')), {
+     person
+    }).then(()=>{
+      console.log("successFull");
     });
 
-    console.log(personDetailList);
-    console.log(personDetail);
+    
   };
 
 
@@ -45,31 +54,34 @@ function App() {
         <input onChange={handleInput} type="text" name='fullname' placeholder='enter youyr name'/>
         <br />
         <label htmlFor="maillAddress">email  : </label>
-        <input onChange={handleInput} type="email" placeholder='Enter your mail' id='maillAddress' name='yourMail'/>
+          <input onChange={handleInput} type="email" placeholder='Enter your mail' id='maillAddress' name='yourMail'/>
         <br />
         <label htmlFor="roll">Roll : </label>
-        <input onChange={handleInput} type="number" placeholder='Enter your roll number' name='roll' id='roll'/>
-        <select onChange={handleInput} name="selectOption" id="selectOption">
+          <input onChange={handleInput} type="number" placeholder='Enter your roll number' name='roll' id='roll'/>
+          <select onChange={handleInput} name="selectOption" id="selectOption">
           <option value="value1">option01</option>
           <option value="value2">option02</option>
           <option value="value3">option03</option>
           <option value="value4">option04</option>
-        </select>
+          </select>
         
         <h4>Intearest In</h4>
 
         <label htmlFor="design">web design</label>
-        <input type="checkbox" name='design' id='design'/>
+        <input onChange={handleInput} type="checkbox" name='design' id='design' value="webdesign"/>
         <br />
         <label htmlFor="development">web development</label>
-        <input type="checkbox" name='development'/>
+        <input onChange={handleInput} type="checkbox" name='development' value="development"/>
         <br />
-        <label htmlFor="">design For develeopment</label>
-        <input type="checkbox" name='development' id=''/>
+        <label htmlFor="development">design For develeopment</label>
+        <input onChange={handleInput} type="checkbox" name='designedToDevolp' id='development' value="designedToDevelop" />
         <br />
 
 
         <button onClick={handleSubmit}>submit</button>
+      </div>
+      <div className="checkedBox">
+
       </div>
     </>
   )
